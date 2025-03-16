09c919c3efcfdb276feb0c76983a3fdf53eedbcf
Vishnu Kartha <vishnukartha1@gmail.com>
2025-03-16 01:47:02 -0700

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://VishnuKartha.github.io',
-  base: '/renoloop',
+  site: 'https://renoloop.com',
+  base: '/',
 });
